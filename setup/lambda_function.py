ca0412307bbee08effbdf382a8122741f0f9e342
majway27 <majway27.2@gmail.com>
2018-02-14 22:19:44 +0000

    Working post for list create

--- setup/lambda_function.py
+++ setup/lambda_function.py
@@ -1,11 +1,14 @@
 # Version 1.2
 
-import os
+import decimal
 import json
 import boto3
-import decimal
+from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
-#lists_table = boto3.resource('dynamodb').Table(os.getenv('TABLE_NAME'))
+import os
+import uuid
+
+
 dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
 table = dynamodb.Table(os.getenv('TABLE_NAME'))
 
@@ -18,15 +21,28 @@ def response_builder(message, status_code):
             'Access-Control-Allow-Origin': '*'
             },
     }
+    
+def query_table(filter_key=None, filter_value=None):
+    """
+    Perform a query operation on the table. Can specify filter_key (col name) and its value to be filtered. Returns the response.
+    """
+    if filter_key and filter_value:
+        filtering_exp = Key(filter_key).eq(filter_value)
+        response = table.query(KeyConditionExpression=filtering_exp)
+        return response
+    else: print("oops") 
+    #return response_builder("Bad Robot", 400)
 
 def lambda_handler(event, context):
-    #print(event)
+    userid = event['requestContext']['authorizer']['claims']['sub']
+    #print(userid)
     
     if event['httpMethod'] == 'GET':
         response_list = []
         response_json = ""
         try:
-            resp = table.scan()
+            resp = query_table("uid",userid)
+            
             for item in resp['Items']:
                 data = {}
                 data["uid"] = item["uid"]
@@ -49,16 +65,15 @@ def lambda_handler(event, context):
         try:
             response = table.put_item(
                 Item={
-                    #'uid': '94776f82-5d24-4c24-b042-a1e1690d2290',
-                    #'listid': 2,
-                    #'name' : 'My List'
-                    'uid': body['uid'],
-                    'listid': int(body['listid']),
-                    'name': body['name']
+                    #'uid': 'bad0b9bb-6af8-4abc-b5ba-1a323733ee45',
+                    'uid': userid,
+                    'listid': str(uuid.uuid4()),
+                    'name': body['name'],
+                    'longDescription': body['longDescription'],
+                    'status': "active"
                 }
             )
         except ClientError as e:
             print(e.response['Error']['Message'])
         else:
-            #print("PutItem Succeeded")
-            return {'statusCode': 200, 'body': 'List Added'}
+            return response_builder('[{"result":"success"}]', 200)
